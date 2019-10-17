1b81aa4d1813797e7c8d326112454f0f84a45764
yoshitakumi <georleve@teiser.gr>
2019-10-17 19:55:21 +0300

    First Commit

---
+++ EasyWay/Models/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyWay.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int Phone { get; set; }
+    }
+}
---
+++ EasyWay/Models/Package.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyWay.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string ReceiverAddress { get; set; }
+        public DateTime DateLeft { get; set; }
+        public int Phone { get; set; }
+    }
+}
